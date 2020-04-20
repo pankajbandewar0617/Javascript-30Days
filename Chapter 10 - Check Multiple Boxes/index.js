8b5454e42647ab98836f3449ac9b87243f82e729
Pankaj Bandewar <pankaj.bandewar76@gmail.com>
2020-04-20 20:15:46 +0530

    Check Multiple CheckBoxes

---
+++ Chapter 10 - Check Multiple Boxes/index.js
@@ -0,0 +1,25 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+console.log(checkboxes);
+
+function handleCheck(e) {
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log('starting to check them in between');
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener('click', handleCheck)
+);
