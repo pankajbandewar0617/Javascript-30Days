d5b5a8319ccc2e1dda157dd5228971f2a8ceb902
Pankaj Bandewar <pankaj.bandewar76@gmail.com>
2020-04-20 20:15:20 +0530

    Dev Tool Ticks

--- Chapter 8 - Fun with HTML5 Canvas/index.js
+++ Chapter 8 - Fun with HTML5 Canvas/index.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.strokeStyle = '#BADASS';
+ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 50;
